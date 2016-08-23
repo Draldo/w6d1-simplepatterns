8aa5f52dabcdafecdb5ff24efdaacb8085507d63
Draldo <draldo_lopez@hotmail.com>
2016-08-22 23:34:04 -0400

    Singleton Factory Observer EditText

--- app/src/main/java/com/example/draldo/simplepatterns/MainActivity.java
+++ app/src/main/java/com/example/draldo/simplepatterns/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.draldo.simplepatterns;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.draldo.simplepatterns.Factory.Animal;
 import com.example.draldo.simplepatterns.Factory.AnimalFactory;
@@ -15,10 +18,16 @@ import rx.Observer;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText mEditText;
+    private TextView mTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mEditText = (EditText) findViewById(R.id.a_main_et);
+        mTextView = (TextView) findViewById(R.id.a_main_text);
 
         Tiger tigerToño = Tiger.getInstance();
         tigerToño.eat();
@@ -44,5 +53,11 @@ public class MainActivity extends AppCompatActivity {
         Observable<String> observable = ObserverHelper.createObservable();
 
         observable.subscribe(observer);
+    }
+
+    public void doMagic(View view) {
+        Observer<String> observer = ObserverHelper.createObserver();
+        String str = mEditText.getText().toString();
+        Observable.just(str).subscribe(observer);
     }
 }
